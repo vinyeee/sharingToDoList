61610fd4d2b34d24ab57dfa9be3bde820e5044bf
vinyeee <loveyebin077@naver.com>
2023-08-10 19:10:51 +0900

    나눠볼 가 first project settings

---
+++ sharingToDoList/src/main/java/dev/namiga/sharingToDoList/SharingToDoListApplication.java
@@ -0,0 +1,13 @@
+package dev.namiga.sharingToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingToDoListApplication.class, args);
+	}
+
+}
---
+++ sharingToDoList/src/test/java/dev/namiga/sharingToDoList/SharingToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.namiga.sharingToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
