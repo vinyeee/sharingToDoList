578feff8470a66677772b6ae2b199f1f8428d52b
PSY222 <ss26348@naver.com>
2023-08-13 13:44:30 +0900

    html white label error디버깅

--- sharingToDoList/src/test/java/dev/namiga/sharingToDoList/SharingToDoListApplicationTests.java
+++ sharingToDoList/src/test/java/dev/namiga/sharingToDoList/SharingToDoListApplicationTests.java
@@ -1,13 +1,26 @@
 package dev.namiga.sharingToDoList;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SharingToDoListApplicationTests {
+//@SpringBootTest
+//class SharingToDoListApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SharingToDoListApplicationTests.class, args);
+//	}
+//}
+@SpringBootApplication
+public class SharingToDoListApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(SharingToDoListApplicationTests.class, args);
 	}
 
 }
